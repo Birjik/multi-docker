009a574d3dd8f5cf6716853bbf7ddbeae3f685ee
Birzhan Auganov <birjik@MacBook-Air-Birzhan.local>
2025-05-20 14:24:46 +0500

    test

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+const axios = require("axios");
  
 const Fib = () => {
   const [seenIndexes, setSeenIndexes] = useState([]);
