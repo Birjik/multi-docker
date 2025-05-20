f56ac133f06d4bab95ea56d4a43f2f1defe2de71
Birzhan Auganov <birjik@MacBook-Air-Birzhan.local>
2025-05-20 19:55:10 +0500

    change description to Fibonacci calculator

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Hello World~
+            Fibonacci calculcator
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
